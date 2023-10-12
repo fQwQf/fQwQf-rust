159950697925c9a319072a7f8c884d21bf537611
fQwQf <supertjz123@foxmail.com>
2023-10-12 09:51:07 +0800

    提交

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let x = 5;
-    let y = &x;
+    let mut s = String::from("hello");
 
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
