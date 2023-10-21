9cab877a454424429248f3384e55159564e44eda
fQwQf <supertjz123@foxmail.com>
2023-10-21 09:51:07 +0800

    提交

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-let mut s = String::from("hello");
-
-let r1 = &s; // 没问题
-let r2 = &s; // 没问题
-let r3 = &mut s; // 大问题
-
-println!("{}, {}, and {}", r1, r2, r3);
+fn main() {
+    let mut s = String::from("hello");
+ 
+     let r1 = &s;
+     let r2 = &s;
+     println!("{} and {}", r1, r2);
+     // 新编译器中，r1,r2作用域在这里结束
+ 
+     let r3 = &mut s;
+     println!("{}", r3);
+ } // 老编译器中，r1、r2、r3作用域在这里结束
+   // 新编译器中，r3作用域在这里结束
