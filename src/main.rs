d428c0b6d2ff299f5936eb941b93b610c042dd49
fQwQf <supertjz123@foxmail.com>
2023-10-27 09:51:07 +0800

    提交

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 fn main() {
-    let mut s = String::from("hello");
- 
-     let r1 = &s;
-     let r2 = &s;
-     println!("{} and {}", r1, r2);
-     // 新编译器中，r1,r2作用域在这里结束
- 
-     let r3 = &mut s;
-     println!("{}", r3);
- } // 老编译器中，r1、r2、r3作用域在这里结束
-   // 新编译器中，r3作用域在这里结束
+    let mut s = String::from("Hello ");
+
+    s.push_str("rust");
+    println!("追加字符串 push_str() -> {}", s);
+
+    s.push('!');
+    println!("追加字符 push() -> {}", s);
+}
