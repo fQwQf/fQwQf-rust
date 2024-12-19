065dd8c20e5addb871efa44dad67fe4dd72197ab
fQwQf <supertjz123@foxmail.com>
2024-12-19 23:02:07 +0800

    修改 main 函数，添加变量 x 的打印与赋值

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-fn greet_world() {
-    let southern_germany = "Grüß Gott!";
-    let chinese = "世界，你好";
-    let english = "World, hello";
-    let regions = [southern_germany, chinese, english];
-    for region in regions.iter() {
-        println!("{}", &region);
-    }
-}
-
 fn main() {
-    greet_world();
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
 }
