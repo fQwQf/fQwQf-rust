12d99eca9300ad6c9f730fd44a818ea2c7c5dde6
fQwQf <supertjz123@foxmail.com>
2024-12-20 20:26:39 +0800

    refactor(src): 更新 main 函数实现
    - 移除原有的字符内存大小打印逻辑
    - 添加新的函数 ret_unit_type 来演示 Rust 中的 if 表达式用法
    - 在 main 函数中调用 ret_unit_type 函数并进行断言

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
-    let x = '中';
-    println!("字符'中'占用了{}字节的内存大小",std::mem::size_of_val(&x));
+    assert_eq!(ret_unit_type(), ())
+}
+
+fn ret_unit_type() {
+    let x = 1;
+    // if 语句块也是一个表达式，因此可以用于赋值，也可以直接返回
+    // 类似三元运算符，在Rust里我们可以这样写
+    let y = if x % 2 == 1 {
+        "odd"
+    } else {
+        "even"
+    };
+    // 或者写成一行
+    let z = if x % 2 == 1 { "odd" } else { "even" };
 }
