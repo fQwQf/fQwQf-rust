c37c136260704d28dc9ac615c8273c9fbfc512af
fQwQf <supertjz123@foxmail.com>
2025-02-05 23:35:18 +0800

    refactor(src): 重构 main 函数以添加字符串长度计算功能并移除元组相关代码

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
-    let tup = (500, 6.4, 1);
+    let s1 = String::from("fsafdsa");
 
-    let (x, y, z) = tup;
+    let (s2, len) = calculate_length(s1);
 
-    println!("The value of y is: {}", y);
+    println!("The length of '{}' is {}.", s2, len);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() 返回字符串的长度
+
+    (s, length)
 }
